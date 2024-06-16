9845674a16bf20f92ae3dff03b4920c09dea7082
brendo98 <brendosilva@tutanota.com>
2024-06-16 20:55:23 -0300

    class about operator

---
+++ type-variable/src/operator.java
@@ -0,0 +1,84 @@
+public class operator {
+    
+    public static void main(String[] args) {
+
+        int number1 = 1;
+        int number2 = 2;
+
+        boolean yesNot = number1 == number2;//???
+        System.out.println("Is number1 egual to number2 ? " + yesNot);
+
+    }
+}
+/*
+        String fullName = "language" + "java";
+
+        System.out.println(fullName);
+
+        String concatenation ="?"; 
+        
+        concatenation = 1+1+1+"1";
+
+        System.out.println(concatenation);
+        
+        concatenation = 1+"1"+1+1;
+
+        System.out.println(concatenation);
+        
+        concatenation = 1+"1"+1+"1";      
+
+        System.out.println(concatenation);
+        
+        concatenation = "1"+1+1+1;
+
+        System.out.println(concatenation);
+        
+        concatenation = "1"+(1+1+1);
+
+        System.out.println(concatenation);
+ */
+
+ /*
+ int number = 5;
+
+number = - number;//number = number * -1; work the same
+System.out.println(number);
+
+number = number * -1;// turn a positive number to negative or the opposite in case the number is negative
+System.out.println(number);
+
+number ++;// number = number + 1;
+System.out.println(number);
+
+System.out.println( ++ number);//System.out.println(number ++); won't work because it will first print number, then increase
+
+
+boolean booleanNumber = false;// set the variable booleanNumber as false
+System.out.println(booleanNumber);// print the false value
+
+System.out.println(!booleanNumber);// it only reverse on the print, the variable remains the same
+System.out.println(booleanNumber);// remains false
+
+booleanNumber = !booleanNumber;// reverse the value of the variable booleanNumber
+System.out.println(booleanNumber);// show the reverse value
+ */
+
+ /*
+ int a, b;
+ a = 5;
+ b = 6;
+ String result = "";
+ if(a==b)
+   result = "true";
+ else
+   result = "false";
+         
+ System.out.println(result);
+
+ result = a==b ? "true" : "false";// same as using IF ELSE
+ System.out.println(result);
+
+ a=b;
+ result = a==b ? "true" : "false";// same as using IF ELSE
+ System.out.println(result);
+*/
