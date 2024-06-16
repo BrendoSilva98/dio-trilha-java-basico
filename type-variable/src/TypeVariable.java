32f4d6f723e6fab534579cacbdc3c36e7eebf180
brendo98 <brendosilva@tutanota.com>
2024-06-15 21:02:53 -0300

    more stuff

--- type-variable/src/TypeVariable.java
+++ type-variable/src/TypeVariable.java
@@ -1,9 +1,11 @@
 public class TypeVariable {
     public static void main(String[] args) throws Exception {
         double salary = 2500;
+        System.out.println(salary);
 
         short shortNumber = 1;
         int normalNumber = shortNumber;
         short shortNumber2 = (short) normalNumber;
+        System.out.println(shortNumber2 );
     }
 }
