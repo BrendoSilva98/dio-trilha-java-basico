9b7a9cda1ffbd7068a6bc995ce2c8725cdfe6f51
brendo98 <brendosilva@tutanota.com>
2024-06-18 21:04:55 -0300

    Add documentation files

---
+++ java-terminal/src/comments.java
@@ -0,0 +1,24 @@
+public class comments {
+    public static void main(String[] args) {
+        // single comment
+
+        /*
+         * multline comment
+         * 
+         * 
+         */
+    }
+
+    /**
+     * Two asterisk is used for documentation
+     * 
+     */
+
+    public void method() {
+
+    }
+    /*
+     * Create documentation fron the terminal line :
+     * javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
+     */
+}
