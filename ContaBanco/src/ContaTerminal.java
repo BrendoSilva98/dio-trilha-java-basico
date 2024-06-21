d09c92622cf4566b2cfbf39567b1bb69015587e3
brendo98 <brendosilva@tutanota.com>
2024-06-21 20:37:37 -0300

    update project bank account

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO:Conhecer e importar a classe Scanner
+        // Exibir as mensagem para o nosso usuario
+        // Obter pela scanner os valores digitados no terminal
+        // Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome !");
+        String cliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite sua Saldo");
+        double saldo = scanner.nextDouble();
+
+        // imprimindo os dados obtidos pelo usuario
+        System.out.print("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia);
+        System.out.println(", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+
     }
 }
-    
