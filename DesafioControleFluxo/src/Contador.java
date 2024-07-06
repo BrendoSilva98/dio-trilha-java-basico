d9e1e3e125a32d7f767cabd09505036195ab3912
brendo98 <brendosilva@tutanota.com>
2024-07-06 17:30:38 -0300

    finish project DesafioControleFluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -7,6 +7,7 @@ public class Contador {
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
+		terminal.close();
 
 		try {
 			// chamando o método contendo a lógica de contagem
@@ -14,10 +15,7 @@ public class Contador {
 
 		} catch (ParametrosInvalidosException exception) {
 			// imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-			//System.out.println("O segundo parâmetro deve ser maior que o primeiro " + exception.getMessage());
-			//System.out.println(exception.getMessage());
 			System.out.println(exception.toString());
-			//exception.printStackTrace();
 		}
 
 	}
@@ -31,7 +29,7 @@ public class Contador {
 		int contagem = parametroDois - parametroUm;
 		// realizar o for para imprimir os números com base na variável contagem
 		for (int i = 0; i < contagem; i++) {
-			System.out.println("Imprimindo o número " + (i+1));
+			System.out.println("Imprimindo o número " + (i + 1));
 		}
 	}
 }
