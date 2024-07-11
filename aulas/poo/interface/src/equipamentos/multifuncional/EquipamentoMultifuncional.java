989fede23777b5446bd132a796bc3ad842040efd
brendo98 <brendosilva@tutanota.com>
2024-07-10 23:57:59 -0300

    interfaces

---
+++ aulas/poo/interface/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -0,0 +1,21 @@
+package equipamentos.multifuncional;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {
+
+    public void copiar() {
+        System.out.println("Copiando via equipamento Multifuncional");
+    }
+
+    public void digitalizar() {
+        System.out.println("Digitalizando via equipamento Multifuncional");
+    }
+
+    public void Imprimir() {
+        System.out.println("Imprimindo via equipamento Multifuncional");
+    }
+
+}
