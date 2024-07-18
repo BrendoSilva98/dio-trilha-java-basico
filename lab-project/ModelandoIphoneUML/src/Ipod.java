034712d50091786160f9965f95474c75f413159e
brendo98 <brendosilva@tutanota.com>
2024-07-17 21:58:52 -0300

    add project folder ModelandoIphoneUML

---
+++ lab-project/ModelandoIphoneUML/src/Ipod.java
@@ -0,0 +1,13 @@
+public class Ipod {
+    public void play(){
+        System.out.println("Playing Music");
+    }
+    public void pause(){
+        System.out.println("Pausing Music");
+    }
+    public void selectMusic(String musica){
+        System.out.println("Selecting Music");
+    }
+    
+
+}
